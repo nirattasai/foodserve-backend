f3ee790ee77cb439f55da0cdf6b0b394ba7bf912
nirattasai <nirattasai@gmail.com>
2022-09-15 14:03:44 +0700

    chore:init project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0;');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
